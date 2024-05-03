d9ebfcd0055fdc87ee99ee9541af817953b48a54
Winston Thai <winstonthai1@gmail.com>
2024-05-03 01:40:13 -0600

    exercise 3.18 - fix info route to work with database

--- index.js
+++ index.js
@@ -23,30 +23,6 @@ const logger = morgan((tokens, request, response) => {
 
 app.use(logger)
 
-let data = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
-
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
@@ -58,7 +34,10 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/info', (request, response) => {
-    response.send(`<p>Phonebook has info for ${data.length} people</p> <p>${new Date().toString()}</p>`)
+    (async() => {
+        const count = await Person.find({}).countDocuments()
+        response.send(`<p>Phonebook has info for ${count} people</p> <p>${new Date().toString()}</p>`)
+    })()
 })
 
 app.get('/api/persons/:id', (request, response, next) => {

6a3cab440900fbf6d1ac6c514a6594887734953d
Winston Thai <winstonthai1@gmail.com>
2024-05-03 00:06:18 -0600

    exercise 3.15 - allow for deletion of items from database

--- index.js
+++ index.js
@@ -67,11 +67,11 @@ app.get('/api/persons/:id', (request, response) => {
     })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    data = data.filter(person => person.id !== Number(id))
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id).then(result => {
+        response.status(204).end()
+    })
+    .catch(error => {next(error)})
 })
 
 app.post('/api/persons', (request, response) => {
